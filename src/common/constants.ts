c510b670e6279113674d5bedb4b8387221b62d9f
Joey Zheng <joey.z.rp@gmail.com>
2019-04-13 21:20:51 +1000

    Fix mixed content

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,4 +3,4 @@ export const BACKGROUND_COLOR = '#081B23';
 export const CONTENT_COLOR = '#97C6D9';
 export const GREEN_COLOR = '#03D177';
 export const HEADER_COLOR = '#E5F7FF';
-export const IMAGE_URL_BASE = 'http://image.tmdb.org/t/p/w';
+export const IMAGE_URL_BASE = 'https://image.tmdb.org/t/p/w';
